effd308716e1838d419de411f4b56fd46595fc6e
nsimon <nsimon@student.42.fr>
2021-02-05 18:59:40 +0100

    00/ex00

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsimon <nsimon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/05 17:19:48 by nsimon            #+#    #+#             */
+/*   Updated: 2021/02/05 18:40:49 by nsimon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < argc; i++)
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)toupper(argv[i][j]);
+	std::cout << std::endl;
+	return 0;
+}
